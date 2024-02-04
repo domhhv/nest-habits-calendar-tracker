59798f298617cb471bc5f24d07c0bbf41fabff1b
Dominik Hryshaiev <domhryshaiev@gmail.com>
2024-02-04 13:02:24 +0200

    Set up migrations (#11)
    * Set up migrations
    
    * Port fix
    
    * Port fix #2
    
    * Package.json fix
    
    * New route

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,9 @@ async function bootstrap() {
       validateCustomDecorators: true,
     }),
   );
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
